1fffe0ccd60de3490958df507efe0021e9a8e6b2
Brian O'Keefe <brian@bokstuff.com>
2014-06-29 14:52:42 -0400

    Remove rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,5 @@
 require 'vhost_writer'
 
-RSpec.configure do |config|
-  # Use color in STDOUT
-  config.color_enabled = true
-
-  # Use color not only in STDOUT but also in pagers and files
-  config.tty = true
-
-  # Use the specified formatter
-  config.formatter = :documentation # :progress, :html, :textmate
-end
-
 # from thor spec helper
 def capture(stream)
   begin
